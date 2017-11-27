23b45f72d489d80f4e8c9fc1bf3c2b26dbf03e9e
ezavile <ezavile@gmail.com>
2017-11-27 16:05:48 -0600

    use while instead of recursive fn due Maximum call stack size exceeded

--- algorithm.js
+++ algorithm.js
@@ -2,14 +2,12 @@ const hash = require('./hash');
 const validateHash = require('./validateHash');
 
 module.exports = function algorithm(proof, message, difficulty, nonce) {
-  if (!validateHash(proof, difficulty)) {
+  while (!validateHash(proof, difficulty)) {
     nonce++;
-    proof = message + nonce;
-    return algorithm(hash.doubleHashing(proof), message, difficulty, nonce);
-  } else {
-    return {
-      hash: proof,
-      nonce,
-    };
+    proof = hash.doubleHashing(message + nonce);
   }
+  return {
+    hash: proof,
+    nonce,
+  };
 }
